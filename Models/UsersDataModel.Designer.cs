d7aac1eb262ef9c572f8f41520fafd78751f7413
koko1313 <kaloyanvellichkov@gmail.com>
2022-01-23 15:44:24 +0200

    adding the new clean users model

--- Models/UsersDataModel.Designer.cs
+++ Models/UsersDataModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'D:\Work Folder\programming-projects\asp.net-login-form\Models\Users.edmx'. 
+﻿// T4 code generation is enabled for model 'D:\Work Folder\programming-projects\asp.net-login-form\Models\UsersDataModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
